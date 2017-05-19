756b142436958fe948b5a9a1784c87a5480a18c7
Mariaguadalupej <guadalupe.jimenez.cbtis16@gmail.com>
2017-05-19 08:43:35 -0500

    6s

--- js/acciones.js
+++ js/acciones.js
@@ -1,28 +1,26 @@
 // JavaScript Document
-// prueba
+
 $(document).ready(function(e) {
-	document.addEventListener("deviceready",function(){
+    document.addEventListener("deviceready",function(){
 		
 		$('#izquierda').on("swipeleft",function(){
-			navigator.notification.alert("Deslizo a la izquieda",function(){"Aplicacion7","Aceptar"});
-			});
+			navigator.notification.alert("Deslizo a la izquierda",function(){"Aplicacion7","Aceptar"});
+});
+
+$('#derecha').on("swiperight",function(){
+	navigator.notification.confirm("¿Que quieres hacer?",function(opt){
+		switch(opt)
+		{
+			case 1:
+			navigator.notification.beep(1);
+			break;
 			
-			$('#derecha').on("swiperight",function(){
-				navigator.notification.confirm("¿Que quieres hacer?",function(opt){
-					switch(opt)
-					{
-						case 1:
-						navigator.notification.beep(1);
-						break;
-						
-						case 2:
-						navigator.notification.vibrate(1000);
-						break;
-						
-					}
-						},"Aplicacion7","Beep,vibrar,cancelar");
-						
-					});
-					},false);
-					});	
-			
+			case 2:
+			navigator.notification.vibrate(1000);
+			break;
+		}
+	},"Aplicacion7","Beep,Vibrar,Cancelar");
+	
+});
+	},false);
+});
